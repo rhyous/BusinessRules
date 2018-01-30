b6f0f6e2589f51a5e619a269022a201540352c51
rhyous <rhyous@yahoo.com>
2018-01-30 09:08:14 -0700

    first checking

---
+++ src/BusinessRules.Shared/BusinessRuleCollectionBase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rhyous.BusinessRules
+{
+    public abstract class BusinessRuleCollectionBase : IBusinessRuleCollection
+    {
+        public abstract string Name { get; }
+
+        public abstract string Description { get; }
+
+        public List<IBusinessRule> Rules
+        {
+            get { return _Rules ?? (_Rules = new List<IBusinessRule>()); }
+            set { _Rules = value; }
+        } private List<IBusinessRule> _Rules;
+
+        public Dictionary<IBusinessRule, BusinessRuleResult> Results
+        {
+            get { return _Results ?? (_Results = new Dictionary<IBusinessRule, BusinessRuleResult>()); }
+            set { _Results = value; }
+        } private Dictionary<IBusinessRule, BusinessRuleResult> _Results;
+
+        public virtual BusinessRuleResult IsMet()
+        {
+            var isMetMethods = new List<Action>();
+            foreach (var rule in Rules)
+            {
+                isMetMethods.Add(() => { Results[rule] = rule.IsMet(); } );
+            }
+            Parallel.Invoke(isMetMethods.ToArray());
+            return new BusinessRuleResult { Result = Results.All(r => r.Value?.Result ?? false), FailedObjects = Results.Select(o => o as object).ToList() };
+        }
+    }
+}
---
+++ src/BusinessRules.Shared/BusinessRuleResult.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Rhyous.BusinessRules
+{
+    public class BusinessRuleResult
+    {
+        public bool Result { get; set; }
+        public List<object> FailedObjects
+        {
+            get { return _FailedObjects ?? (_FailedObjects = new List<object>()); }
+            set { _FailedObjects = value; }
+        } private List<object> _FailedObjects;
+
+        public void AddFailedObject(object o)
+        {
+            if (!FailedObjects.Contains(o))
+                FailedObjects.Add(o);
+        }
+    }
+}
